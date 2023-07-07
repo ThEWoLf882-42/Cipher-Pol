486c0468d0792f77a7de93b9769b75c0d70af0c1
ThEWoLf882-42 <agimi@student.1337.ma>
2023-07-07 12:20:56 +0100

    brain

--- CP1/ex02/main.cpp
+++ CP1/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 21:51:49 by agimi             #+#    #+#             */
-/*   Updated: 2023/07/06 21:53:19 by agimi            ###   ########.fr       */
+/*   Updated: 2023/07/07 12:20:27 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,17 @@ int	main()
 {
 	std::string	string;
 	std::string	*stringPTR;
-	
+	std::string	&stringREF = string;
+
+	string = "HI THIS IS BRAIN";
+	stringPTR = &string;
+
+	std::cout << "string    %p: " << &string << std::endl;
+	std::cout << "stringPTR %p: " << stringPTR << std::endl;
+	std::cout << "stringREF %p: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "string    %s: " << string << std::endl;
+	std::cout << "stringPTR %s: " << *stringPTR << std::endl;
+	std::cout << "stringREF %s: " << stringREF << std::endl;
 }
