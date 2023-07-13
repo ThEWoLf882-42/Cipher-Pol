fe2753b2270dc5a9554b775375afa619fc4d985f
ThEWoLf882-42 <agimi@student.1337.ma>
2023-07-13 15:07:05 +0100

    bsp skeleton

---
+++ CP2/ex03/Fixed.cpp
@@ -0,0 +1,167 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 15:05:13 by agimi             #+#    #+#             */
+/*   Updated: 2023/07/13 15:05:14 by agimi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+const int	Fixed::frb = 8;
+
+Fixed::Fixed(void) :
+	fpn(0)
+{
+}
+
+Fixed::~Fixed(void)
+{
+}
+
+Fixed::Fixed(const int f) :
+	fpn(f << frb)
+{
+}
+
+Fixed::Fixed(const float f) :
+	fpn(roundf(f * (1 << frb)))
+{
+}
+
+Fixed::Fixed(Fixed const &f)
+{
+	*this = f;
+}
+
+Fixed	&Fixed::operator=(Fixed const &f)
+{
+	setRawBits(f.getRawBits());
+	return *this;
+}
+
+int		Fixed::getRawBits(void) const
+{
+	return this->fpn;
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->fpn = raw;
+}
+
+float	Fixed::toFloat(void) const
+{
+	return ((float)fpn / (float)(1 << frb));
+}
+
+int		Fixed::toInt(void) const
+{
+	return (fpn >> frb);
+}
+
+
+bool	Fixed::operator>(Fixed const &f)
+{
+	return	toFloat() > f.toFloat();
+}
+
+bool	Fixed::operator<(Fixed const &f)
+{
+	return	toFloat() < f.toFloat();
+}
+
+bool	Fixed::operator>=(Fixed const &f)
+{
+	return	toFloat() >= f.toFloat();
+}
+
+bool	Fixed::operator<=(Fixed const &f)
+{
+	return	toFloat() <= f.toFloat();
+}
+
+bool	Fixed::operator==(Fixed const &f)
+{
+	return	toFloat() == f.toFloat();
+}
+
+bool	Fixed::operator!=(Fixed const &f)
+{
+	return	toFloat() != f.toFloat();
+}
+
+float	Fixed::operator+(Fixed const &f)
+{
+	return	toFloat() + f.toFloat();
+}
+
+float	Fixed::operator-(Fixed const &f)
+{
+	return	toFloat() - f.toFloat();
+}
+
+float	Fixed::operator*(Fixed const &f)
+{
+	return	toFloat() * f.toFloat();
+}
+
+float	Fixed::operator/(Fixed const &f)
+{
+	return	toFloat() / f.toFloat();
+}
+
+Fixed	Fixed::operator++()
+{
+	fpn++;
+	return *this;
+}
+
+Fixed	Fixed::operator--()
+{
+	fpn--;
+	return *this;
+}
+
+Fixed	Fixed::operator++(int)
+{
+	Fixed	t = *this;
+	fpn++;
+	return t;
+}
+
+Fixed	Fixed::operator--(int)
+{
+	Fixed	t = *this;
+	fpn--;
+	return t;
+}
+
+const Fixed	&Fixed::min(Fixed const &fi,Fixed const &se)
+{
+	return (Fixed)fi <= (Fixed)se ? fi : se;
+}
+
+Fixed	&Fixed::min(Fixed &fi, Fixed &se)
+{
+	return fi <= se ? fi : se;
+}
+
+const Fixed	&Fixed::max(Fixed const &fi,Fixed const &se)
+{
+	return (Fixed)fi >= (Fixed)se ? fi : se;
+}
+
+Fixed	&Fixed::max(Fixed &fi, Fixed &se)
+{
+	return fi >= se ? fi : se;
+}
+
+std::ostream	&operator<<(std::ostream &str, Fixed const &fn)
+{
+	return str << fn.toFloat();
+}
---
+++ CP2/ex03/Fixed.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 15:05:18 by agimi             #+#    #+#             */
+/*   Updated: 2023/07/13 15:05:20 by agimi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <cmath>
+# include <iostream>
+
+class Fixed
+{
+private:
+	int					fpn;
+	static const int	frb;
+public:
+	Fixed(void);
+	Fixed(const int f);
+	Fixed(const float f);
+	~Fixed(void);
+	Fixed(Fixed const &f);
+	Fixed	&operator=(Fixed const &f);
+	
+	bool	operator>(Fixed const &f);
+	bool	operator<(Fixed const &f);
+	bool	operator>=(Fixed const &f);
+	bool	operator<=(Fixed const &f);
+	bool	operator==(Fixed const &f);
+	bool	operator!=(Fixed const &f);
+	
+	float	operator+(Fixed const &f);
+	float	operator-(Fixed const &f);
+	float	operator*(Fixed const &f);
+	float	operator/(Fixed const &f);
+	
+	Fixed	operator++();
+	Fixed	operator--();
+	Fixed	operator++(int);
+	Fixed	operator--(int);
+
+	static Fixed		&min(Fixed &fi, Fixed &se);
+	static const Fixed	&min(Fixed const &fi,Fixed const &se);
+	static Fixed		&max(Fixed &fi, Fixed &se);
+	static const Fixed	&max(Fixed const &fi,Fixed const &se);
+	
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+	float	toFloat(void) const;
+	int		toInt(void) const;
+};
+
+std::ostream	&operator<<(std::ostream &str, Fixed const &fn);
+
+#endif
