f4aff87a667cee377f513778f9c09d41525bd95d
ThEWoLf882-42 <agimi@student.1337.ma>
2023-07-25 17:14:48 +0100

    CP3

--- CP3/ex01/main.cpp
+++ CP3/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 17:12:50 by agimi             #+#    #+#             */
-/*   Updated: 2023/07/18 17:16:18 by agimi            ###   ########.fr       */
+/*   Updated: 2023/07/25 17:13:22 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 int main()
 {
-	
+	std::cout << "\n\n### TESTING SCAVTRAP ###\n" << std::endl;
+	std::cout << "\033[34mConstructing\033[0m" << std::endl;
+	ScavTrap a;
+	ScavTrap b("Giga Chadd");
+	ScavTrap c(a);
+	a.guardGate();
+	a.attack("some super random dude");
+	b.guardGate();
+	b.attack("Chadd-clone");
+	c.guardGate();
+	return (0);
 }
--- CP3/ex03/main.cpp
+++ CP3/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 18:36:25 by agimi             #+#    #+#             */
-/*   Updated: 2023/07/25 16:47:02 by agimi            ###   ########.fr       */
+/*   Updated: 2023/07/25 17:11:36 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,76 +14,15 @@
 
 int main()
 {
-	// std::cout << "### TESTING CLAPTRAP ###\n" << std::endl;
-	// {
-	// 	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	// 	ClapTrap a;
-	// 	ClapTrap b("Cody");
-
-	// 	std::cout << "\033[34mTesting\033[0m" << std::endl;
-	// 	a.attack("some other robot");
-	// 	a.takeDamage(10);
-	// 	a.takeDamage(10);
-	// 	a.beRepaired(5);
-	// 	a.attack("some other other robot");
-	// 	b.beRepaired(3);
-	// 	for (int i = 0; i < 12; i++)
-	// 		b.attack("Cody-clone");
-	// 	b.beRepaired(3);
-	// 	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	// }
-	// std::cout << "\n\n### TESTING SCAVTRAP ###\n" << std::endl;
-	// {
-	// 	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	// 	ScavTrap a;
-	// 	ScavTrap b("Savage");
-
-	// 	std::cout << "\033[34mTesting\033[0m" << std::endl;
-	// 	a.attack("CloneTrap");
-	// 	// for (int i = 0; i < 50; i++)
-	// 	// 	a.attack("CloneTrap");
-	// 	a.beRepaired(22);
-	// 	a.takeDamage(21);
-	// 	a.beRepaired(22);
-	// 	a.guardGate();
-	// 	a.guardGate();
-	// 	b.attack("Savage-clone");
-	// 	b.takeDamage(101);
-	// 	b.takeDamage(15);
-	// 	b.attack("ScavTrap-clone");
-	// 	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	// }
-	// std::cout << "\n\n### TESTING FRAGTRAP ###\n" << std::endl;
-	// {
-	// 	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	// 	FragTrap a;
-	// 	FragTrap b("Chadd");
-
-	// 	std::cout << "\033[34mTesting\033[0m" << std::endl;
-	// 	a.highFivesGuys();
-	// 	a.attack("some random dude");
-	// 	a.takeDamage(101);
-	// 	a.takeDamage(1);
-	// 	a.attack("some random dude");
-	// 	b.highFivesGuys();
-	// 	// for(int i = 0; i < 101; i++)
-	// 	// 	b.attack("FragTrap-clone");
-	// 	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	// }
 	std::cout << "\n\n### TESTING DIAMONDTRAP ###\n" << std::endl;
-	{
-		std::cout << "\033[34mConstructing\033[0m" << std::endl;
-		DiamondTrap a;
-		DiamondTrap b("Giga Chadd");
-		DiamondTrap c(a);
-
-		// std::cout << "\033[34mTesting\033[0m" << std::endl;
-		a.whoAmI();
-		a.attack("some super random dude");
-		b.whoAmI();
-		// b.attack("Chadd-clone");
-		c.whoAmI();
-		// std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	}
+	std::cout << "\033[34mConstructing\033[0m" << std::endl;
+	DiamondTrap a;
+	DiamondTrap b("Giga Chadd");
+	DiamondTrap c(a);
+	a.whoAmI();
+	a.attack("some super random dude");
+	b.whoAmI();
+	b.attack("Chadd-clone");
+	c.whoAmI();
 	return (0);
 }
