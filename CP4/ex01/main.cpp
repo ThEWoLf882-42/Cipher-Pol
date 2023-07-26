7cdcf08162fcc05bda9c2c2796c5ffef886e57a8
ThEWoLf882-42 <agimi@student.1337.ma>
2023-07-26 16:13:11 +0100

    main

--- CP4/ex01/main.cpp
+++ CP4/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 10:05:38 by agimi             #+#    #+#             */
-/*   Updated: 2023/07/26 15:49:45 by agimi            ###   ########.fr       */
+/*   Updated: 2023/07/26 16:13:02 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,94 +16,5 @@
 
 int main()
 {
-	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	const Animal	*meta[10];
-	for (int i = 0; i < 10; i++)
-	{
-		if (i % 2)
-		{
-			meta[i] = new Cat();
-			if (meta[i] == NULL)
-			{
-				perror("Cat allocation failed");
-				std::cerr << "Exiting process now";
-				exit(1);
-			}
-		}
-		else
-		{
-			meta[i] = new Dog();
-			if (meta[i] == NULL)
-			{
-				perror("Dog allocation failed");
-				std::cerr << "Exiting process now";
-				exit(1);
-			}
-		}
-	}
-	std::cout << std::endl;
-
-	std::cout << "\033[34mTesting\033[0m" << std::endl;
-	for (int i = 0; i < 10; i++)
-	{
-		std::cout << std::endl;
-		std::cout << "Animal _type: " << meta[i]->getType() << std::endl;
-		meta[i]->makeSound();
-		std::cout << std::endl;
-	}
-	std::cout << std::endl;
-
-	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	for (int i = 0; i < 10; i++)
-		delete(meta[i]);
-
-//THIS PART IS FOR TESTING DEEP COPY ↓
-
-	std::cout << std::endl << std::endl;
-	std::cout << "#### showing that the copy constructor creates a deep copy ####" << std::endl;
-	std::cout << std::endl;
-
-	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	Dog *a = new Dog();
-	// Cat *a = new Cat();
-	if (a == NULL)
-	{
-		perror("Allocation failed");
-		std::cerr << "Exiting the process now." << std::endl;
-		exit(1);
-	}
-
-	a->setIdea(0, "I have to sniff it");
-	a->setIdea(1, "I have to pee on it");
-	a->setIdea(2, "I have to sniff it again");
-	a->setIdea(101, "some shit");
-
-	Dog *b = new Dog(*a);
-	// Cat *b = new Cat(*a);
-	if (b == NULL)
-	{
-		perror("Allocation failed");
-		std::cerr << "Exiting the process now." << std::endl;
-		exit(1);
-	}
-	std::cout << std::endl;
-
-	std::cout << "\033[34mTesting a\033[0m" << std::endl;
-	std::cout << "The " << a->getType() << " a has the following ideas: " << std::endl;
-	a->getIdeas();
-	std::cout << std::endl;
-
-	std::cout << "\033[34mDeconstructing a\033[0m" << std::endl;
-	delete(a);
-	std::cout << std::endl;
-
-	std::cout << "\033[34mTesting b\033[0m" << std::endl;
-	std::cout << "The " << b->getType() << " b has the following ideas: " << std::endl;
-	b->getIdeas();
-	std::cout << std::endl;
-
-	std::cout << "\033[34mDeconstructing b\033[0m" << std::endl;
-	delete(b);
-
-	return (0);
+	
 }
