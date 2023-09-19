94a7bc4a5b34790d2e6443e4016401e9598062ca
ThEWoLf882-42 <agimi@student.1337.ma>
2023-09-19 19:54:12 +0100

    iter fixed

--- CP7/ex01/iter.hpp
+++ CP7/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 11:18:55 by agimi             #+#    #+#             */
-/*   Updated: 2023/09/18 11:28:56 by agimi            ###   ########.fr       */
+/*   Updated: 2023/09/19 19:01:07 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@
 
 template<typename A>
 void	iter(A *ar, size_t l, void (*f)(A &))
+{
+	for (size_t i = 0; i < l; i++)
+		f(ar[i]);
+}
+
+template<typename A>
+void	iter(A *ar, size_t l, void (*f)(A const &))
 {
 	for (size_t i = 0; i < l; i++)
 		f(ar[i]);
