75544ac5a695f26512de543e52c7a4877a39efae
ThEWoLf882-42 <agimi@student.1337.ma>
2023-09-24 12:36:16 +0100

    CP9/ex00

--- CP9/ex00/BitcoinExchange.cpp
+++ CP9/ex00/BitcoinExchange.cpp
@@ -6,8 +6,239 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 17:44:34 by agimi             #+#    #+#             */
-/*   Updated: 2023/09/21 17:45:01 by agimi            ###   ########.fr       */
+/*   Updated: 2023/09/24 12:15:53 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange()
+{
+}
+
+BitcoinExchange::BitcoinExchange(std::string const &f) :
+	file(f)
+{
+	if (file.is_open())
+	{
+		init_data();
+		read_input();
+	}
+	else
+	{
+		std::cerr << "Unable to open file" << std::endl;
+		exit(1);
+	}
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+	file.close();
+}
+
+BitcoinExchange::BitcoinExchange(BitcoinExchange const &b)
+{
+	*this = b;
+}
+
+BitcoinExchange	&BitcoinExchange::operator=(BitcoinExchange const &b)
+{
+	if (this != &b)
+		m = b.m;
+	return	*this;
+}
+
+double	BitcoinExchange::gprice(std::string da)
+{
+	std::map<std::string, double>::iterator it = m.lower_bound(da);
+	if (it != m.begin()) {
+		if (it->first != da)
+			--it;
+		return it->second;
+	}
+	return it->second;
+}
+
+int BitcoinExchange::check_date(const std::string& da)
+{
+	if (da.length() != 10 || da[4] != '-' || da[7] != '-')
+		return 1;
+
+	int	year_i = std::atoi(da.substr(0, 4).c_str());
+	int	month_i = std::atoi(da.substr(5, 2).c_str());
+	int	day_i = std::atoi(da.substr(8, 2).c_str());
+
+	std::time_t	t = std::time(0);
+	std::tm		*now = std::localtime(&t);
+	int			year_now = now->tm_year + 1900;
+
+	if (year_i > year_now || year_i < 2009)
+		return 1;
+
+	if (month_i < 1 || month_i > 12)
+		return 1;
+
+	int days_in_month = month_date(year_i, month_i);
+
+	if (day_i < 1 || day_i > days_in_month)
+		return 1;
+
+	return 0;
+}
+
+int BitcoinExchange::month_date(int year, int month)
+{
+	static const int	daysInMonth[] = 
+						{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	int days = daysInMonth[month];
+
+	if (month == 2 && (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)))
+		days = 29;
+
+	return days;
+}
+
+int	BitcoinExchange::line_check(std::string const &l, int in)
+{
+	std::string da;
+	std::string pr;
+
+	std::istringstream ss(l);
+
+	if (in == 0)
+		ss >> da >> pr >> pr;
+	else 
+	{
+		std::getline(ss, da, ',');
+		std::getline(ss, pr, ',');
+		
+		da.erase(std::remove(da.begin(), da.end(), ' '), da.end());
+		pr.erase(std::remove(pr.begin(), pr.end(), ' '), pr.end());
+	}
+
+	if (da.empty() || pr.empty())
+		throw std::invalid_argument("Error: Bad input => " + (da.empty() ? pr : da));
+
+	try
+	{
+		char* not_digit;
+		std::strtod(da.c_str(), &not_digit);
+		if (*not_digit != '\0' && *not_digit != '-')
+			throw std::invalid_argument("Error: Bad date input");
+		
+		if (check_date(da))
+			throw std::invalid_argument("Error: Bad date input");
+		
+		char	*crumbs;
+		double	pr_d = std::strtod(pr.c_str(), &crumbs);
+		if (*crumbs != '\0')
+			throw std::invalid_argument("Error: Bad price input");
+
+		if (pr_d < 0)
+			throw std::invalid_argument("Error: Not a positive number");
+
+		if (pr_d > 1000 && in == 0)
+			throw std::invalid_argument("Error: Too large a number");
+	}
+	catch (const std::invalid_argument& e)
+	{
+		throw std::invalid_argument(std::string(e.what()));
+	}
+
+	return 0;
+}
+
+void	BitcoinExchange::parse(std::string const l)
+{
+	line_check(l, 1);
+	std::stringstream	s(l);
+	std::string			da, pr;
+
+	if (std::getline(s, da, ',') && std::getline(s, pr, ','))
+	{
+		da.erase(std::remove(da.begin(), da.end(), ' '), da.end());
+		pr.erase(std::remove(pr.begin(), pr.end(), ' '), pr.end());
+		m.insert(std::make_pair(da, std::strtod(pr.c_str(), NULL)));
+	}
+	else 
+	{
+		throw std::runtime_error("Invalid data format in line: " + l);
+	}
+}
+
+void	BitcoinExchange::init_data()
+{
+	std::ifstream	data("data.csv");
+	if (!data.is_open())
+	{
+		std::cerr << "Error: could not open database file." << std::endl;
+		exit(1);
+	}
+
+	std::string l;
+	if (std::getline(data, l) && l != "date,exchange_rate")
+	{
+		std::cerr << "Error: Bad file format" << std::endl;
+		exit(1);
+	}
+	
+	while (std::getline(data, l))
+	{
+		try
+		{
+			parse(l);
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << "Data error: " << e.what() << std::endl;
+			exit(1);
+		}
+	}
+
+	data.close();
+}
+
+void	BitcoinExchange::read_input()
+{
+	std::string l;
+	
+	if (file.is_open())
+	{
+		std::getline(file, l);
+		if (l != "date | value")
+		{
+			std::cerr << "Error: Bad file format" << std::endl;
+			exit(1);
+		}
+		while (std::getline(file, l))
+		{
+			try
+			{
+				line_check(l, 0);
+
+				std::stringstream	ss(l);
+				std::string			da;
+				std::string			pr;
+				ss >> da;
+				ss >> pr;
+				ss >> pr;
+
+				std::cout << da << " => " << pr << " = ";
+				double	t = gprice(da);
+
+				std::cout << t * std::strtod(pr.c_str(), NULL) << std::endl;
+			}
+			catch (const std::exception& e)
+			{
+				std::cerr << e.what() << '\n';
+			}
+		}
+		file.close();
+	}
+	else
+	{
+		std::cerr << "Unable to open file" << std::endl;
+		exit(1);
+	}
+}
--- CP9/ex00/BitcoinExchange.hpp
+++ CP9/ex00/BitcoinExchange.hpp
@@ -6,8 +6,37 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 17:44:42 by agimi             #+#    #+#             */
-/*   Updated: 2023/09/21 17:44:50 by agimi            ###   ########.fr       */
+/*   Updated: 2023/09/24 12:03:01 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <stdexcept>
+
+class	BitcoinExchange
+{
+	private:
+		std::ifstream					file;
+		std::map<std::string, double>	m;
+		BitcoinExchange();
+		void	init_data();
+		void	read_input();
+		void	parse(std::string const l);
+		int		line_check(std::string const &l, int in);
+		int		month_date(int year, int month);
+		int		check_date(const std::string& da);
+		double	gprice(std::string da);
+	public:
+		BitcoinExchange(std::string	const &f);
+		~BitcoinExchange();
+		BitcoinExchange(BitcoinExchange const &b);
+		
+		BitcoinExchange	&operator=(BitcoinExchange const &b);
+
+};
