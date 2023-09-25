1f9c8054139e80451271db0a9bf13ae37b0eeaa2
ThEWoLf882-42 <agimi@student.1337.ma>
2023-09-25 14:42:55 +0100

    inverted Polish mathematical expression

--- CP9/ex00/main.cpp
+++ CP9/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 14:48:40 by agimi             #+#    #+#             */
-/*   Updated: 2023/09/25 14:42:21 by agimi            ###   ########.fr       */
+/*   Updated: 2023/09/25 14:42:46 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ int	main(int ac, char **av)
 	if	(ac == 2)
 		BitcoinExchange	bit(av[1]);
 	else
-		std::cerr << "usage: ./btc [inverted Polish mathematical expression]" << std::endl;
+		std::cerr << "usage: ./btc [file]" << std::endl;
 }
--- CP9/ex01/main.cpp
+++ CP9/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 15:18:39 by agimi             #+#    #+#             */
-/*   Updated: 2023/09/25 14:38:35 by agimi            ###   ########.fr       */
+/*   Updated: 2023/09/25 14:42:51 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ int	main(int ac, char **av)
 	if	(ac == 2)
 		RPN	rpn(av[1]);
 	else
-		std::cerr << "usage: ./RPN [expresion]" << std::endl;
+		std::cerr << "usage: ./RPN [inverted Polish mathematical expression]" << std::endl;
 }
